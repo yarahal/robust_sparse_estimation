1a8e4a61ae065dbe3496b441270676d073667689
Yara Halawi <yarahalawi12@gmail.com>
2022-11-12 11:42:54 -0500

    Restructured code

--- sayed.py
+++ sayed.py
@@ -1,63 +1,76 @@
 import numpy as np
 from math import sqrt
 
-# define network
-N = 20;
-A = np.random.randint(0,5,size=(N,N)) # A(l,k): weight of edge directed from l to k
-A = A / np.sum(A,axis=0)
 
-# define actual model
-M = 20;
-w = np.random.randint(low=0,high=10,size=(M,1))
-# make w sparse
-# mask = np.random.randint(low=0,high=2,size=(M,1))
-# w = w * mask
+class Network:
+    def set_random_network(self,N):
+        # define network
+        self.N = N;
+        self.A = np.random.randint(0,5,size=(N,N)) # A(l,k): weight of edge directed from l to k
+        A = A / np.sum(A,axis=0)
+    
+    def set_random_model(self,M):
+        self.M = M
+        self.w = np.random.randint(low=0,high=10,size=(M,1))
+        return self.w
+    
+    def make_model_sparse(self):
+        mask = np.random.randint(low=0,high=2,size=(self.M,1))
+        self.w = self.w * mask
+        return self.w
 
-#  collect data from every agent
-#  every agent probes the model
-var_v = 0.1
-Nt = 100
-U = np.zeros(shape=(N,Nt)) # for recording agent inputs
-D = np.zeros(shape=(N,Nt)) # for recording agent outputs
-for k in range(0,N):
-    uk = np.random.randn(1,Nt) # generate input according to standard gaussian
-    dk = np.zeros(shape=(1,Nt)) 
-    vk = sqrt(var_v)*np.random.randn(1,Nt)
-    for i in range(M,Nt):
-        uki = np.flip(uk[:,i-M+1:i+1],axis=1)
-        dk[:,i] = uki @ w + vk[:,i] # convolve and add white gaussian noise
-    U[k,:] = uk
-    D[k,:] = dk
+    def collect_data(self,var_v,Nt):
+        #  collect data from every agent
+        #  every agent probes the model
+        self.var_v = var_v
+        self.Nt = Nt
+        self.U = np.zeros(shape=(self.N,Nt)) # for recording agent inputs
+        self.D = np.zeros(shape=(self.N,Nt)) # for recording agent outputs
+        for k in range(0,self.N):
+            uk = np.random.randn(1,Nt) # generate input according to standard gaussian
+            dk = np.zeros(shape=(1,Nt)) 
+            vk = sqrt(var_v)*np.random.randn(1,Nt)
+            for i in range(self.M,Nt):
+                uki = np.flip(uk[:,i-self.M+1:i+1],axis=1)
+                dk[:,i] = uki @ self.w + vk[:,i] # convolve and add white gaussian noise
+            self.U[k,:] = uk
+            self.D[k,:] = dk
+    
+    def atc(self,mu):
+        # diffusion-adaptation
+        self.mu = mu
+        w_est_prev = np.random.randn(self.N,self.M)
+        phi = np.copy(w_est_prev)
+        w_est = np.zeros(shape=(self.N,self.M)) # hold estimates
 
-# diffusion-adaptation
-mu = 0.1
-w_est_prev = np.random.randn(N,M)
-phi = np.copy(w_est_prev)
-w_est = np.zeros(shape=(N,M)) # hold estimates
+        for i in range(self.M,self.Nt):
+            for k in range(0,self.N):
+                w_est[k,:] = np.zeros(shape=(1,self.M))
+                uki = np.expand_dims(np.flip(self.U[k,i-self.M+1:i+1]),0)  
+                phi[k,:] = w_est_prev[k,:] + mu* np.squeeze(uki.transpose() @ (self.D[k,i] - uki @ np.expand_dims(w_est_prev[k,:],-1)))      
+                for l in range(0,self.N):
+                    w_est[k,:] += self.A[l,k] * phi[l,:]
+                w_est_prev[k,:] = w_est[k,:]
+        
+        return w_est
+    
+    def atc_sparsity(self,mu,lamda):
+        # diffusion-adaptation with sparsity
+        self.mu = mu
+        self.lamda = lamda # regularization parameter
+        w_est_prev = np.random.randn(self.N,self.M)
+        phi = np.copy(w_est_prev)
+        w_est = np.zeros(shape=(self.N,self.M)) # hold estimates
 
-for i in range(M,Nt):
-    for k in range(0,N):
-        w_est[k,:] = np.zeros(shape=(1,M))
-        uki = np.expand_dims(np.flip(U[k,i-M+1:i+1]),0)  
-        phi[k,:] = w_est_prev[k,:] + mu* np.squeeze(uki.transpose() @ (D[k,i] - uki @ np.expand_dims(w_est_prev[k,:],-1)))      
-        for l in range(0,N):
-            w_est[k,:] += A[l,k] * phi[l,:]
-        w_est_prev[k,:] = w_est[k,:]
-            
-
-# diffusion-adaptation with sparsity
-mu = 0.1
-lamda = 1e-3 # regularization parameter
-w_est_prev = np.random.randn(N,M)
-w_est = np.zeros(shape=(N,M)) # hold estimates
-
-for i in range(M,Nt):
-    for k in range(0,N):
-        phi = np.zeros(shape=(M,1));
-        for l in range(0,N):
-            phi = phi + A[l,k] * np.expand_dims(w_est_prev[l,:],-1)
-        uki = np.expand_dims(np.flip(U[k,i-M+1:i+1]),0)
-        reg_term = np.diag(1/(np.abs(w_est_prev[k,:])+1e-2)) @ np.sign(np.expand_dims(w_est_prev[l,:],-1))
-        w_est_prev[k,:] = w_est[k,:]
-        w_est[k,:] = np.squeeze(phi + mu* uki.transpose() @ (D[k,i] - uki @ phi) - mu*lamda*reg_term)
-
+        for i in range(self.M,self.Nt):
+            for k in range(0,self.N):
+                w_est[k,:] = np.zeros(shape=(1,self.M))
+                uki = np.expand_dims(np.flip(self.U[k,i-self.M+1:i+1]),0)  
+                reg_term = np.diag(1/(np.abs(w_est_prev[k,:])+1e-2)) @ np.sign(np.expand_dims(w_est_prev[l,:],-1))
+                phi[k,:] = w_est_prev[k,:] + mu* np.squeeze(uki.transpose() @ (self.D[k,i] - uki @ np.expand_dims(w_est_prev[k,:],-1)))  - mu*lamda*reg_term    
+                for l in range(0,self.N):
+                    w_est[k,:] += self.A[l,k] * phi[l,:]
+                w_est_prev[k,:] = w_est[k,:]
+        
+        return w_est
+    
